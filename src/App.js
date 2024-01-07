259d579721b7c2351debc09cfd609a7bdc910b35
Sanjeev Kumar <sanjeevcse2k23@gmail.com>
2024-01-07 19:32:40 +0530

    Committing changes in submodule

--- src/App.js
+++ src/App.js
@@ -11,12 +11,14 @@
 
 import React, { useState, useEffect } from 'react';
 import { database } from './config/firebase';
-import { collection, addDoc, getDocs } from 'firebase/firestore';
+import { collection, addDoc, getDocs, deleteDoc, doc, updateDoc } from 'firebase/firestore';
 import './App.css';
 
 function App() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [Id, setId] = useState('');
+  const [show, setShow] =useState(false)
   const [val, setVal] = useState([]);
 
   const value = collection(database, 'demo');
@@ -31,6 +33,25 @@ function App() {
 
   const handleCreate = async () => {
     await addDoc(value, { Name: name, Email: email });
+    setEmail("")
+    setName("")
+  };
+  const handleDelete = async (id) => {
+   const deleteVal= doc(database, "demo", id)
+   await deleteDoc(deleteVal)
+  };
+  const handleEdit = async (id, name, email) => {
+    setEmail(email)
+    setName(name)
+    setShow(true)
+    setId(id)
+  };
+  const handleUpdate = async () => {
+    const updateData = doc(database, "demo", Id)
+    await updateDoc(updateData, { Name: name, Email: email });
+    setShow(false)
+    setEmail("")
+    setName("")
   };
 
   return (
@@ -39,7 +60,8 @@ function App() {
         <h2>Contact List</h2>
         <input value={name} onChange={e => setName(e.target.value)} type="text" placeholder="Enter Name" />
         <input value={email} onChange={e => setEmail(e.target.value)} type="email" placeholder="Enter Email" />
-        <button onClick={handleCreate}>Create</button>
+        {!show? <button onClick={handleCreate}>Create</button>
+        : <button onClick={handleUpdate}>Update</button>}
         </div>
         
         <div className='data-box'>
@@ -48,6 +70,10 @@ function App() {
           <div key={values.id} className='data'>
             <h3>Name: {values.Name}</h3>
             <h3>Email: {values.Email}</h3>
+           <span className='update-delete'> 
+            <button onClick={()=> handleDelete(values.id)} >Delete</button>
+            <button onClick={()=> handleEdit(values.id, values.Name, values.Email )} >Edit</button>
+            </span>
           </div>
         ))}
       </div>
